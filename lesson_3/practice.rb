e612c1fb4635a7e09338187bba84b9347660bf37
David Navarro <dmn88mia@gmail.com>
2017-01-24 16:17:42 -0500

    updated practice file

--- lesson_3/practice.rb
+++ lesson_3/practice.rb
@@ -71,7 +71,15 @@
 # puts calculate_bonus(50000, true) == 25000
 
 
-def calculate_bonus(salary, bonus)
-  salary / 2 if bonus == true
+
+# { a: "ant", b: "bear", c: "cat" }.any? do |key, value|
+#   value.size > 4
+# end
+def fl
+['ant', 'bat', 'caterpillar'].count do |str|
+  str.length < 4
 end
-puts calculate_bonus(50000, true)
+end
+end
+
+p fl
